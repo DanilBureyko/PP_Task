9f4483880a53f36f0aece08beba9b7cf3bc985a2
DanilBureyko <116087625+DanilBureyko@users.noreply.github.com>
2025-02-10 15:49:32 +0300

    PP_task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,26 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 32);
+        userService.saveUser("Sergey", "Sergeev", (byte) 58);
+        userService.saveUser("Vasya", "Vasyeliev", (byte) 24);
+        userService.saveUser("Kirill", "Kirilov", (byte) 18);
+
+        userService.removeUserById(2);
+
         userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
 
 
     }
